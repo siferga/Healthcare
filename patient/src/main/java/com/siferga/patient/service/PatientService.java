43566bbce87216452ace85fc0850e419a46a80aa
Siferga <sifergadeveloper@gmail.com>
2024-06-10 11:29:23 +0200

    Patient Service

---
+++ patient/src/main/java/com/siferga/patient/service/PatientService.java
@@ -0,0 +1,35 @@
+package com.siferga.patient.service;
+
+import com.siferga.patient.model.Patient;
+import com.siferga.patient.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public Patient save(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public List<Patient> findAll() {
+        return patientRepository.findAll();
+    }
+
+    public Optional<Patient> findById(Integer id) {
+        return patientRepository.findById(id);
+    }
+
+    public void delete(Patient patient) {
+        patientRepository.delete(patient);
+    }
+}
