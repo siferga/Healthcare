1a473551e966b2d6d38f2556d56e6ac7ea8a92d3
Siferga <sifergadeveloper@gmail.com>
2024-06-10 13:27:40 +0200

    PatientController webapp

--- patient/src/main/resources/schema.sql
+++ patient/src/main/resources/schema.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS `patient` (
 );
 CREATE TABLE IF NOT EXISTS `user` (
   `userid` int AUTO_INCREMENT  PRIMARY KEY,
-  `username` varchar(255) NOT NULL UNIQUE,
+  `email` varchar(255) NOT NULL UNIQUE,
   `password` varchar(255)
 );
