1a710922fb5116b2364157b36358028c4ada3b9e
Siferga <sifergadeveloper@gmail.com>
2024-06-10 11:10:29 +0200

    PatientApplication feign and jpa beans

--- patient/src/main/java/com/siferga/patient/PatientApplication.java
+++ patient/src/main/java/com/siferga/patient/PatientApplication.java
@@ -2,8 +2,14 @@ package com.siferga.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaRepositories("com.siferga.patient.repository")
+@EntityScan("com.siferga.patient.model")
 public class PatientApplication {
 
 	public static void main(String[] args) {
