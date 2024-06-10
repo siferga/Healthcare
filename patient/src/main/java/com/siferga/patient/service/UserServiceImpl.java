fb6f1259baf5994f8a80fed1c5b3a92b19d2f2ed
Siferga <sifergadeveloper@gmail.com>
2024-06-10 11:35:42 +0200

    UserServiceImpl

---
+++ patient/src/main/java/com/siferga/patient/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.siferga.patient.service;
+
+import com.siferga.patient.model.User;
+import com.siferga.patient.repository.UserRepository;
+import com.siferga.webapp.service.form.SignUpForm;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registration(User user) {
+        return userRepository.save(user);
+    }
+}
