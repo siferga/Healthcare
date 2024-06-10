20dbc627254116a88279440dbaacefdaf1c23ac8
Siferga <sifergadeveloper@gmail.com>
2024-06-10 11:51:38 +0200

    bdd

---
+++ patient/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `patient` (
+  `patientid` int AUTO_INCREMENT  PRIMARY KEY,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS `user` (
+  `userid` int AUTO_INCREMENT  PRIMARY KEY,
+  `username` varchar(255) NOT NULL UNIQUE,
+  `password` varchar(255)
+);
