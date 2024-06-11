9379fa0b22a2095488167008cff5e335daee1051
Siferga <sifergadeveloper@gmail.com>
2024-06-11 12:04:23 +0200

    Patient model webapp update

--- webapp/src/main/java/com/siferga/webapp/model/Patient.java
+++ webapp/src/main/java/com/siferga/webapp/model/Patient.java
@@ -1,25 +1,21 @@
 package com.siferga.webapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
 
-@Entity
+import java.time.LocalDate;
+
+//@Entity
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Patient {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String patientid;
+    private Long patientid;
     private String firstname;
     private String lastname;
     private String gender;
-    private String birthdate;
+    private LocalDate birthday;
     private String address;
     private String phone;
     private String email;
