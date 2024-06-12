0dbca74caeaa1659e99af168eac3f58d9fa3ae9b
Siferga <sifergadeveloper@gmail.com>
2024-06-12 12:44:01 +0200

    pom and appli delete useless beans

--- patient/src/main/java/com/siferga/patient/PatientApplication.java
+++ patient/src/main/java/com/siferga/patient/PatientApplication.java
@@ -2,14 +2,9 @@ package com.siferga.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableJpaRepositories("com.siferga.patient.repository")
-@EntityScan("com.siferga.patient.model")
 public class PatientApplication {
 
 	public static void main(String[] args) {
