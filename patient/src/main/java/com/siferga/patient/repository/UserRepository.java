d944b47095881c9611e38e6c03cc16806621fc3a
Siferga <sifergadeveloper@gmail.com>
2024-06-12 12:35:21 +0200

    UserRepo update

--- patient/src/main/java/com/siferga/patient/repository/UserRepository.java
+++ patient/src/main/java/com/siferga/patient/repository/UserRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByUsername(String username);
+    User findUserByEmail(String email);
 
 }
